cd8bc3b867933dcfde5023e4c1f2f7c2a3a48ce7
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-02-28 23:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function mincost(arr)
 { 
 //write your code here
+	let minHeap = new MinHeap();
+	for (let i = 0; i < arr.length; i++) {
+		minHeap.add(arr[i]);
+	}
+	let cost = 0;
+	while (minHeap.size()>1) {
+		let min1 = minHeap.extractMin();
+		let min2 = minHeap.extractMin();
+		let sum = min1 + min2;
+		cost += sum;
+		minHeap.add(sum);
+	}
 // return the min cost
-  
+  return cost;
 }
 
 module.exports=mincost;
